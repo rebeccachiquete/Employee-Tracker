53cbe3f5230a890774a7a143a0a9ff351e24ec90
Rebecca Chiquete <deerinra194@gmail.com>
2021-02-09 15:30:57 -0700

    added employees function working

--- employeeTracker.js
+++ employeeTracker.js
@@ -36,6 +36,7 @@ function begin() {
         "Exit",
       ]
     })
+
     .then(function(answer) {
       switch (answer.action) {
       case "Add Employee":
@@ -101,127 +102,49 @@ function viewDepartment() {
   }
 
 
-// function addEmployee() {
-//   inquirer
-//     .prompt({
-//       name: "artist",
-//       type: "input",
-//       message: "What artist would you like to search for?"
-//     })
-//     .then(function(answer) {
-//       var query = "SELECT position, song, year FROM top5000 WHERE ?";
-//       connection.query(query, { artist: answer.artist }, function(err, res) {
-//         for (var i = 0; i < res.length; i++) {
-//           console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
-//         }
-//         runSearch();
-//       });
-//     });
-// }
+function addDepartment() {
+  inquirer
+    .prompt({
+      name: "newDepartment",
+      type: "input",
+      message: "What Department Would You Like To Add?"
+    })
+    .then(function(answer) {
+      var query = "INSERT INTO department (dept_name) VALUES (?) ";
+      connection.query(query, answer.newDepartment , function(err, res) {
+        if (err) throw err;
+        viewDepartment();
+      });
+    });
+}
 
 
-// function rangeSearch() {
-//   inquirer
-//     .prompt([
-//       {
-//         name: "start",
-//         type: "input",
-//         message: "Enter starting position: ",
-//         validate: function(value) {
-//           if (isNaN(value) === false) {
-//             return true;
-//           }
-//           return false;
-//         }
-//       },
-//       {
-//         name: "end",
-//         type: "input",
-//         message: "Enter ending position: ",
-//         validate: function(value) {
-//           if (isNaN(value) === false) {
-//             return true;
-//           }
-//           return false;
-//         }
-//       }
-//     ])
-//     .then(function(answer) {
-//       var query = "SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?";
-//       connection.query(query, [answer.start, answer.end], function(err, res) {
-//         for (var i = 0; i < res.length; i++) {
-//           console.log(
-//             "Position: " +
-//               res[i].position +
-//               " || Song: " +
-//               res[i].song +
-//               " || Artist: " +
-//               res[i].artist +
-//               " || Year: " +
-//               res[i].year
-//           );
-//         }
-//         runSearch();
-//       });
-//     });
-// }
 
-// function songSearch() {
-//   inquirer
-//     .prompt({
-//       name: "song",
-//       type: "input",
-//       message: "What song would you like to look for?"
-//     })
-//     .then(function(answer) {
-//       console.log(answer.song);
-//       connection.query("SELECT * FROM top5000 WHERE ?", { song: answer.song }, function(err, res) {
-//         console.log(
-//           "Position: " +
-//             res[0].position +
-//             " || Song: " +
-//             res[0].song +
-//             " || Artist: " +
-//             res[0].artist +
-//             " || Year: " +
-//             res[0].year
-//         );
-//         runSearch();
-//       });
-//     });
-// }
+function addEmployee() {
+    inquirer
+      .prompt( [{
+        name: "employeeName",
+        type: "input",
+        message: "What is the employee name?"
+      },
 
-// function songAndAlbumSearch() {
-//   inquirer
-//     .prompt({
-//       name: "artist",
-//       type: "input",
-//       message: "What artist would you like to search for?"
-//     })
-//     .then(function(answer) {
-//       var query = "SELECT top_albums.year, top_albums.album, top_albums.position, top5000.song, top5000.artist ";
-//       query += "FROM top_albums INNER JOIN top5000 ON (top_albums.artist = top5000.artist AND top_albums.year ";
-//       query += "= top5000.year) WHERE (top_albums.artist = ? AND top5000.artist = ?) ORDER BY top_albums.year, top_albums.position";
+      {
+        name: "employeeLastName",
+        type: "input",
+        message: "What is the employee last name?"
+      },
 
-//       connection.query(query, [answer.artist, answer.artist], function(err, res) {
-//         console.log(res.length + " matches found!");
-//         for (var i = 0; i < res.length; i++) {
-//           console.log(
-//             i+1 + ".) " +
-//               "Year: " +
-//               res[i].year +
-//               " Album Position: " +
-//               res[i].position +
-//               " || Artist: " +
-//               res[i].artist +
-//               " || Song: " +
-//               res[i].song +
-//               " || Album: " +
-//               res[i].album
-//           );
-//         }
-
-//         runSearch();
-//       });
-//     });
-// }
+      {
+        name: "employeeRoleId",
+        type: "input",
+        message: "What is the employee role ID?"
+      },
+    ])
+      .then(function(answer) {
+        var query = "INSERT INTO employee (first_name, last_name, role_id) VALUES (?,?,?)";
+        connection.query(query, [answer.employeeName, answer.employeeLastName, answer.employeeRoleId] , function(err, res) {
+          if (err) throw err;
+          viewEmployee();
+        });
+      });
+  }
